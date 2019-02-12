c99c3e0a368ee62ccc05155cf55594fde0571946
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-12 17:00:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 # your code goes here
+def begins_with_r(array)
+  array.each { |word| return false if word.start_with?("r") == false }
+  true
+end
+
+def contain_a(array)
+  array.select { |word| word.include?("a")}
+end
+
+def first_wa(array)
+  array.detect { |word| word.to_s.start_with?("wa") }
+end
+
+def remove_non_strings(array)
+  array.delete_if { |element| element.class != String }
+end
